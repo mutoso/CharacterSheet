25bd9dfb85360d93723086046474e4e671db3231
Alastair Feille <me@alastair.se>
2024-01-27 23:31:45 -0600

    Added Part 1

--- Assets/Scripts/CharacterSheet.cs
+++ Assets/Scripts/CharacterSheet.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterSheet : MonoBehaviour
 {
+    [SerializeField] string characterName = "John D'oh";
+    [SerializeField] int proficiencyBonus = 2;
+    [SerializeField] bool usingFinesseWeapon = true;
+    [SerializeField][Range(-5, 5)] int strengthModifier = 1;
+    [SerializeField][Range(-5, 5)] int dexterityModifier = 3;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int hitModifier = (usingFinesseWeapon ? dexterityModifier : strengthModifier) + proficiencyBonus;
+        Debug.Log(characterName + "'s hit modifier is " + hitModifier + ".");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
