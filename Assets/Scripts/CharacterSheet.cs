8b82cd75d3ece99c1ae83fb401a75c329d36c3ab
Alastair Feille <me@alastair.se>
2024-01-27 23:43:36 -0600

    Added Part 2

--- Assets/Scripts/CharacterSheet.cs
+++ Assets/Scripts/CharacterSheet.cs
@@ -12,7 +12,32 @@ public class CharacterSheet : MonoBehaviour
     void Start()
     {
         int hitModifier = (usingFinesseWeapon ? dexterityModifier : strengthModifier) + proficiencyBonus;
-        Debug.Log(characterName + "'s hit modifier is " + hitModifier + ".");
+        Debug.Log($"{characterName}'s hit modifier is {hitModifier}.");
+
+        int enemyAC = Random.Range(10, 20 + 1);
+        Debug.Log($"Enemy's AC is {enemyAC}.");
+
+        int diceRoll = Random.Range(1, 20 + 1);
+        Debug.Log($"{characterName} rolls 1d20. It lands on {diceRoll}.");
+
+        Debug.Log($"{diceRoll} + {hitModifier} = {diceRoll + hitModifier} vs. {enemyAC}");
+        if (diceRoll + hitModifier >= enemyAC)
+        {
+            // It hits!
+            if (usingFinesseWeapon)
+            {
+                Debug.Log("You swiftly swing your sword and land a hit on your opponent!");
+            }
+            else
+            {
+                Debug.Log("You slam your club against the foe. It crumbles under its weight.");
+            }
+        }
+        else
+        {
+            // It misses!
+            Debug.Log("The enemy is too quick and dodges your weapon!");
+        }
     }
 
     // Update is called once per frame
